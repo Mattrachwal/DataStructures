db3bca3dfba3e76b10c9eedf57eb22ca6bf0569f
mattrachwal <mattrachwal@gmail.com>
2021-11-09 19:17:18 -0600

    added more

---
+++ Section5/index.js
@@ -0,0 +1,22 @@
+// ========================================================
+// Paterns
+// ========================================================
+
+
+/**
+ * 
+ * Frequency Counter
+ * 
+ * Multiple Pointers
+ * 
+ * Sliding Window
+ * 
+ * Divide and Conquer
+ * 
+ * Dynamic Programming
+ * 
+ * Greedy Algorithms
+ * 
+ * Backtracking
+ * 
+ */
